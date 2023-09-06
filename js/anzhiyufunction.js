b3a13b52d9454a4067a1f2c4a0d530ea1b0f79b5
Rlan-h <2163036955@qq.com>
2023-09-06 12:19:29 +0800

    Site updated: 2023-09-06 12:19:28

--- js/anzhiyufunction.js
+++ js/anzhiyufunction.js
@@ -7,9 +7,3 @@ function musicBindEvent() {
     anzhiyu.musicToggle(false);
   });
 }
-
-// const metingJs = document.querySelector("#nav-music meting-js");
-// //判断是否是音乐
-// if (metingJs.contains(event.target)) {
-// } else {
-// }
